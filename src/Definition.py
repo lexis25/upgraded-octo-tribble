a26aed27cb5ef3179f59c4bac65dcf223bb1a839
alehandro kotovski <tokallex@gmail.com>
2022-06-06 22:03:23 +0300

    debug lubanovich

--- src/Definition.py
+++ src/Definition.py
@@ -1,7 +1,10 @@
 # hausaufgabe 9.1
 def good():
     return ['Harry', 'Ron', 'Hermione']
-print(good())
+
+
+# print(good())
+
 
 # hausaufgabe 9.2
 def get_odds():
@@ -10,6 +13,30 @@ def get_odds():
         if number % 2 != 0:
             count += 1
             if count == 3:
-                yield number
+                return number
+
 
 print(get_odds())
+
+# hausaufgabe 9.3
+def test():
+    def start_test():
+        print('start')
+    start_test()
+    print('end')
+
+test()
+
+# hausaufgabe 9.4 (p/s i hate Bill Lubanovich MZFK(((( )
+class OopsException(Exception):
+    pass
+numbers = [1,2]
+for index in numbers:
+    if index == 3:
+        print('Caught.an.oops')
+        raise OopsException(index)
+
+try:
+    raise OopsException(3)
+except OopsException as exc:
+    print(exc)
